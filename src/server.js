b29b3186da9d00dadf5fb70fd7c48d64357e9798
joaoizidioLoiola <joaoizidio287@gmail.com>
2024-08-21 20:36:41 -0300

    Atts

--- src/server.js
+++ src/server.js
@@ -4,27 +4,17 @@ import productRouter from './routers/productRouter.js'
 import createProd from './controllers/products/createProd.js'
 import editNameProd from './controllers/products/editNameProd.js'
 import editProd from './controllers/products/editProd.js'
+import { PORT, HOST, ENVIRONMENT } from './config.js'
 
 const app = express()
-const port = 3000
 app.use(express.json());
 
 app.use('/user', userRouter)
 app.use('/prods', productRouter)
-// app.put('/prods', editProd)
-// app.post('/prods', createProd)
-// app.patch('/prods/:idProd', editNameProd)
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`)
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando no ambiente ${ENVIRONMENT} em ${ENVIRONMENT == 'production' ? HOST : HOST + ':' + PORT}`)
 })
-// app.get('/', (req, res) => {
-//   res.send('Olá Mundo!')
-// })
 
-// app.get('/user1', (req, res) => {
-//   res.json({
-//     nome: "João", idade: "21", email: "joaoizidioloiola@icloud.com"
-//   })
-// })
 
