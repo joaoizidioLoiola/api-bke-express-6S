f59f1b4e52f4285693c707b323db94c2b141c365
joaoizidioLoiola <joaoizidio287@gmail.com>
2024-08-21 22:01:42 -0300

    Atualizando prodList.js

--- src/controllers/products/prodList.js
+++ src/controllers/products/prodList.js
@@ -3,7 +3,7 @@ import prodModel from "../../models/prodModel.js"
 const prodList = async (req, res) => {
   const prods = await prodModel.getAllProds()
   if (!prods) {
-    return res.status(404).json({ message: 'Lista de Produtos vazia' });
+    return res.status(404).json({ error: 'Lista de Produtos vazia' });
   }
   res.json(prods)
 }
