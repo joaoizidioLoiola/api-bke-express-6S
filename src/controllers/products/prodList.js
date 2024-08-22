905a6953e7195fe6f23b32ae54de4eb6c96af4fa
joaoizidioLoiola <joaoizidio287@gmail.com>
2024-08-21 21:47:53 -0300

    Editando prodList

--- src/controllers/products/prodList.js
+++ src/controllers/products/prodList.js
@@ -2,6 +2,9 @@ import prodModel from "../../models/prodModel.js"
 
 const prodList = async (req, res) => {
   const prods = await prodModel.getAllProds()
+  if (prods == null) {
+    return res.status(404).json({ message: 'Lista de Produtos vazia' });
+  }
   res.json(prods)
 }
 export default prodList
