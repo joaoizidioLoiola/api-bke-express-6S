1cd0c58709e1d254501309a6618696cf4a72dde6
João Izídio <88620101+joaoizidioLoiola@users.noreply.github.com>
2024-09-11 20:33:03 -0300

    Add logger middleware to log requests

--- src/server.js
+++ src/server.js
@@ -1,17 +1,22 @@
 import express from 'express'
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
+import logger from './middlewares/logger.js'
+import welcome from './welcome.js'
+import routeNotFounded from './routeNotFounded.js'
 import { PORT, HOST, ENVIRONMENT } from './config.js'
 
 const app = express()
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Bem vindo a API de Produtos e Usuários' })
-})
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Bem vindo a API de Produtos e Usuários' })
+// })
 
+app.length('/', welcome)
 app.use('/user', userRouter)
-app.use('/prods', productRouter)
+app.use('/product', productRouter)
+app.use('*', routeNotFounded)
 
 
 app.listen(PORT, () => {
