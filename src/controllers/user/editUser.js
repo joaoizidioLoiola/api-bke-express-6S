8375351044546c973e587b4638514d83093ca164
joaoizidioloiola <joaoizidio287@gmail.com>
2025-08-02 11:50:42 -0300

    Update dependencies in package-lock.json, modify Prisma schema for user and product models, enhance editUser controller, and refactor server setup with improved routing and error handling.

--- src/controllers/user/editUser.js
+++ src/controllers/user/editUser.js
@@ -4,7 +4,6 @@ const editUser = async (req, res) => {
   const { id } = req.params;
   const { nameUser, emailUser, pass } = req.body;
 
-<<<<<<< HEAD
   if (!id || !nameUser || !emailUser || !pass) {
     return res.status(400).json({ message: 'Todos os campos são necessários' });
   }
@@ -13,21 +12,6 @@ const editUser = async (req, res) => {
     const updateUser = await userModel.editUser(id, nameUser, emailUser, pass);
     if (!updateUser) {
       return res.status(404).json({ message: 'Usuario não encontrado' });
-=======
-  // if (!idUser || !nameUser || !emailUser || !pass) {
-  //   return res.status(400).json({ message: 'Todos os campos são necessários' });
-  // }
-
-  try {
-    const updateUser = await userModel.editUser(idUser, nameUser, emailUser, pass);
-    const userValidade = ValidateUserToUpdate(updateUser);
-
-    if (userValidade?.error) {
-      return res.status(400).json({
-        error: "Erro ao atualizar usuário, verifique os dados",
-        fieldErrors: userValidade.error.flatten().fieldErrors
-      });
->>>>>>> f84a56aac7a1d7dad7da184b416d800fc2fbb7dc
     }
 
 
--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import cors from 'cors';
 import productRouter from './routers/productRouter.js';
@@ -6,25 +5,9 @@ import userRouter from './routers/userRouter.js'; // Adicione esta importação
 import { PORT, HOST, ENVIRONMENT } from './config.js';
 
 const app = express();
-=======
-import express from 'express'
-import userRouter from './routers/userRouter.js'
-import productRouter from './routers/productRouter.js'
-import logger from './middlewares/logger.js'
-import welcome from './welcome.js'
-import routeNotFounded from './routeNotFounded.js'
-import { PORT, HOST, ENVIRONMENT } from './config.js'
-import errorHandler from './middlewares/errorHandler.js'
-
-const app = express()
-
-app.use(logger)
-
->>>>>>> f84a56aac7a1d7dad7da184b416d800fc2fbb7dc
 app.use(express.json());
 app.use(cors());
 
-<<<<<<< HEAD
 app.get('/', (req, res) => {
   res.json({ message: 'Bem vindo a API de Produtos e Usuários' });
 });
@@ -35,15 +18,3 @@ app.use('/prods', productRouter);
 app.listen(PORT, () => {
   console.log(`Servidor rodando no ambiente ${ENVIRONMENT} em ${ENVIRONMENT === 'production' ? HOST : `${HOST}:${PORT}`}`);
 });
-=======
-app.get('/', welcome)
-app.use('/user', userRouter)
-app.use('/prods', productRouter)
-app.use('*', routeNotFounded)
-
-app.use(errorHandler)
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando no ambiente ${ENVIRONMENT} em ${ENVIRONMENT == 'production' ? HOST : HOST + ':' + PORT}`)
-})
->>>>>>> f84a56aac7a1d7dad7da184b416d800fc2fbb7dc
